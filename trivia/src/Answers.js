da5ce98bd41057ed6982447fce619d36133bc1fe
aidanflinton <69019017+aidanflinton@users.noreply.github.com>
2022-04-18 13:11:55 -0400

    Buttons and random question order, they just dont do anything yet

--- trivia/src/Answers.js
+++ trivia/src/Answers.js
@@ -2,9 +2,24 @@ import React from "react"
 import { useState , useEffect} from "react"
 
 const Answers = (props) => {
+  const ansArray = [];
+  for(let i = 0; i < props.wrongAns.length; i++){
+    ansArray[i] = props.wrongAns[i];
+  }
+
+  let swapIndex = parseInt((props.wrongAns.length) * Math.random());
+  if(swapIndex !== props.wrongAns.length){
+      ansArray[props.wrongAns.length] =  ansArray[swapIndex];
+  }
+  ansArray[swapIndex] = props.rightAns;  
+
   return (
     <>
-      <p>{props.rightAns + "," + props.wrongAns}</p>
+      <div>
+        {ansArray.map((item) => {
+          return <button>{item}</button>
+        })}
+      </div>
     </>
   )
 }
