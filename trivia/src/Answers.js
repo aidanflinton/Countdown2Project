eb2b50cbd2c589b8d82d5b974e2021531e7e90db
aidanflinton <69019017+aidanflinton@users.noreply.github.com>
2022-04-19 13:13:48 -0400

    Buttons give feedback when clicked, but not influenced by correct answer. Questions are stored in mui acordions

--- trivia/src/Answers.js
+++ trivia/src/Answers.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { useState , useEffect} from "react"
+import { useState , useEffect} from "react";
+import Button from '@mui/material/Button';
 
 const Answers = (props) => {
   const ansArray = [];
@@ -7,17 +8,33 @@ const Answers = (props) => {
     ansArray[i] = props.wrongAns[i];
   }
 
-  let swapIndex = parseInt((props.wrongAns.length) * Math.random());
+  let swapIndex = parseInt((props.wrongAns.length + 1) * Math.random());
   if(swapIndex !== props.wrongAns.length){
       ansArray[props.wrongAns.length] =  ansArray[swapIndex];
   }
   ansArray[swapIndex] = props.rightAns;  
 
+  const [style, setStyle] = useState("primary");
+  
+  let changeStyle = () => {
+    setStyle("success");
+    /*
+    if(value === props.rightAns)
+      setStyle("success");
+    else{
+      setStyle("error");
+    }
+    */
+  };
+
   return (
     <>
       <div>
         {ansArray.map((item) => {
-          return <button>{item}</button>
+          return(
+          <Button variant="contained" color={style} onClick={changeStyle}> 
+            {item} 
+          </Button>)
         })}
       </div>
     </>
--- trivia/src/Questions.js
+++ trivia/src/Questions.js
@@ -2,11 +2,29 @@ import React from "react"
 import { useState , useEffect} from "react"
 import Answers from "./Answers.js"
 
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 const Questions = (props) => {
   return (
     <>
-      <h4>{props.qstn}</h4>
-      {<Answers rightAns={props.info.correct_answer} wrongAns={props.info.incorrect_answers}/>}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{props.qstn}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            {<Answers rightAns={props.info.correct_answer} wrongAns={props.info.incorrect_answers}/>}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </>
   )
 }
@@ -14,3 +32,5 @@ const Questions = (props) => {
 export default Questions;
 
 //<h1>{props.questions}</h1>
+//<h4>{props.qstn}</h4>
+      //{<Answers rightAns={props.info.correct_answer} wrongAns={props.info.incorrect_answers}/>}
